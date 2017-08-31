5b5df1163bf63cb88abc3b28c3e166a25e7d4000
Edgardo Torres Vanden Bouch <etvb66@hotmail.com>
2017-08-31 05:52:07 -0500

    agregamos las variables, pedimos los datos y procesamos

--- PATS-17-7-CalcularSemanasDiasHoras/src/pats/pkg17/pkg7/calcularsemanasdiashoras/PATS177CalcularSemanasDiasHoras.java
+++ PATS-17-7-CalcularSemanasDiasHoras/src/pats/pkg17/pkg7/calcularsemanasdiashoras/PATS177CalcularSemanasDiasHoras.java
@@ -5,6 +5,8 @@
  */
 package pats.pkg17.pkg7.calcularsemanasdiashoras;
 
+import java.util.Scanner;
+
 /**
  *
  * @author edgardo
@@ -16,6 +18,25 @@ public class PATS177CalcularSemanasDiasHoras {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Scanner teclado = new Scanner(System.in);
+        int horasTotales, semanas, dias, horas;
+        
+        System.out.print("Ingresa el numero de HORAS TOTALES: ");
+        horasTotales = teclado.nextInt();
+        
+        //La semana tiene 168 horas
+        //7 dias * 24 hrs de un día = 168 horas
+        
+        semanas = horasTotales / 168;
+        dias = horasTotales%168 / 24;
+        horas = horasTotales%24;
+        
+        System.out.println("Semanas = " + semanas);
+        System.out.println("Días = " + dias);
+        System.out.println("Horas = " + horas);
+        
+        
     }
     
 }

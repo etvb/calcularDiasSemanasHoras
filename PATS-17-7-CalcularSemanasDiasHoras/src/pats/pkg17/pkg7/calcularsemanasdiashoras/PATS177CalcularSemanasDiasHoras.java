b59e578e2172d54835c566ff32ea024ac287c996
Edgardo Torres Vanden Bouch <etvb66@hotmail.com>
2017-08-31 05:37:03 -0500

    archivos base

---
+++ PATS-17-7-CalcularSemanasDiasHoras/src/pats/pkg17/pkg7/calcularsemanasdiashoras/PATS177CalcularSemanasDiasHoras.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pats.pkg17.pkg7.calcularsemanasdiashoras;
+
+/**
+ *
+ * @author edgardo
+ */
+public class PATS177CalcularSemanasDiasHoras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
